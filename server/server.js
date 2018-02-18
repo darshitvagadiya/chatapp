4da8e7ccbae17e1f627fa14a95bc702484b7eaa0
darshit soni <darshitvagadiya@yahoo.com>
2018-02-18 18:11:05 +0530

    adding connected and disconnection of socket

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 var express = require('express');
+var http = require('http');
 var bodyParser = require('body-parser');
 var path = require('path');
+var socketIO = require('socket.io');
+
 var app = express();
 var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port, function(){
+io.on('connection', (socket) => {
+	console.log('new user connected');
+
+	socket.on('disconnect', () => {
+		console.log('user is disconnected from client side');
+	});
+})
+
+server.listen(port, function(){
 	console.log(`Express server listening on ${port}`);
-});
+}); 
